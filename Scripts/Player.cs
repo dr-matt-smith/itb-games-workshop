82e67b12c7a8da885d4abd24d52764006fc72938
dr-matt-smith <dr_matt_smith@me.com>
2018-06-06 15:02:15 +0100

    added files to repo

---
+++ Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	public static int score = 0;
+
+	private void OnTriggerEnter(Collider hit)
+	{
+		if (hit.CompareTag("Food"))
+		{
+			Player.score++;
+			Destroy(hit.gameObject);
+		}
+	}
+
+	private void Update()
+	{
+		print("Score: " + Player.score);
+	}
+}
